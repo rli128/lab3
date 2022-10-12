e5c6fa1e491f443a82dc246fa117b2fcbe2bcae0
rli128 <114562309+rli128@users.noreply.github.com>
2022-10-12 14:42:09 -0700

    fix

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+	public void testReverseInPlaceRicky() {
+    int[] input1 = { 3, 2, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 4, 2, 3 }, input1);
+	}
+
+  @Test 
+	public void testReverseRicky() {
+    int[] input1 = { 1, 2 };
+    ArrayExamples.reversed(input1);
+    assertArrayEquals(new int[]{ 2, 1 }, ArrayExamples.reversed(input1));
+	}
 }
