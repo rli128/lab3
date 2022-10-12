593aae4ad643c1666628f294c98ed818545d9766
rli128 <114562309+rli128@users.noreply.github.com>
2022-10-12 15:47:42 -0700

    fixes

--- ListTests.java
+++ ListTests.java
@@ -1,19 +1,32 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListTests {
 	@Test 
 	public void testFilter() {
     StringChecker sc = new StringChecker() {
 
-        @Override
         public boolean checkString(String s) {
+            if (s.length() > 5) {
+                return true;
+            }
+
             return false;
         }
+
     };
-    List<String> input1 = new ArrayList<>();
-    ListExamples.filter(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    List<String> input1 = new ArrayList<String>();
+    input1.add("Banana");
+    input1.add("Red");
+    input1.add("Motorcycle");
+
+    List<String> output = new ArrayList<String>();
+    output.add("Banana");
+    output.add("Motorcycle");
+
+    assertEquals(output, ListExamples.filter(input1, sc));
 	}
     
 }
